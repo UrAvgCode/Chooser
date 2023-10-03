bc902b4a939ddd7b06a29996159c6e67004467cc
UrAvgCode <138674451+UrAvgCode@users.noreply.github.com>
2023-10-03 14:04:21 +0200

    adjustments to Number class

--- app/src/main/java/com/uravgcode/chooser/utils/Number.kt
+++ app/src/main/java/com/uravgcode/chooser/utils/Number.kt
@@ -1,23 +1,37 @@
 package com.uravgcode.chooser.utils
 
 import android.graphics.Paint
-import kotlin.math.abs
+import kotlin.math.max
 import kotlin.math.sin
 
-class Number(var x: Float, var y: Float, color: Int, val number: Int, private val size: Float) {
-    var alpha = 120f
+class Number(var x: Float, var y: Float, color: Int, val number: Int, private var size: Float) {
+
     val textPaint = Paint()
 
+    val originX = x
+
+    var alpha = 125f
+    var alphaSpeed = 0f
+
+    var time = 0
+
     init {
+        size = 140f
         textPaint.color = color
         textPaint.textAlign = Paint.Align.CENTER
         textPaint.textSize = size
     }
 
     fun updateValues(deltaTime: Int) {
+        alphaSpeed += deltaTime * 0.00003f
+        alpha = max(alpha - alphaSpeed * deltaTime, 0f)
+        alphaSpeed += deltaTime * 0.00003f
+
         textPaint.alpha = alpha.coerceIn(0f, 100f).toInt()
-        alpha -= (deltaTime * 3f) / abs(alpha)
-        x += sin(alpha / 15f) * deltaTime * 0.1f
-        y -= deltaTime * 0.06f
+
+        x = originX + sin(time * 0.003).toFloat() * size * 0.25f
+        y -= size * deltaTime * 0.0005f
+
+        time += deltaTime
     }
 }
