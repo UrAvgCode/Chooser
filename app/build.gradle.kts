14867e1af5d570ab050cbd201bf6f538a21c967e
Your Average Code <138674451+UrAvgCode@users.noreply.github.com>
2024-01-05 10:40:19 +0100

    material:1.11.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -42,6 +42,6 @@ android {
 dependencies {
     implementation("androidx.core:core-ktx:1.12.0")
     implementation("androidx.appcompat:appcompat:1.6.1")
-    implementation("com.google.android.material:material:1.10.0")
+    implementation("com.google.android.material:material:1.11.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
 }
