3f5186b8b68c3af3b1e123ab827c2f81aaae0f6b
Your Average Code <138674451+UrAvgCode@users.noreply.github.com>
2024-01-28 06:41:14 +0100

    set include dependenciesInfo to false

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -30,10 +30,16 @@ android {
             )
         }
     }
+
+    dependenciesInfo {
+        includeInApk = false
+    }
+
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_1_8
         targetCompatibility = JavaVersion.VERSION_1_8
     }
+
     kotlinOptions {
         jvmTarget = "1.8"
     }
