230d1bb8d0ada75c0b799057085aad902b5fa819
Your Average Code <138674451+UrAvgCode@users.noreply.github.com>
2024-01-28 13:18:11 +0100

    version bump

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,8 +15,8 @@ android {
         applicationId = "com.uravgcode.chooser"
         minSdk = 26
         targetSdk = 34
-        versionCode = 3
-        versionName = "1.2-pre-1"
+        versionCode = 4
+        versionName = "1.2"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
