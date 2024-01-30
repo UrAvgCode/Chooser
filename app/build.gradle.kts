2e51095b5409039fd6f8deba2d4d8590b7a61a06
Your Average Code <138674451+UrAvgCode@users.noreply.github.com>
2024-01-30 11:42:59 +0100

    androidx.activity dependency

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -47,6 +47,6 @@ android {
 }
 
 dependencies {
-    implementation("androidx.core:core-ktx:1.12.0")
+    implementation("androidx.activity:activity-ktx:1.8.2")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
 }
