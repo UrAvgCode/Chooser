7589907b84a998cbde2209b0539e0e36f316fb37
Your Average Code <138674451+UrAvgCode@users.noreply.github.com>
2024-02-02 23:37:56 +0100

    fix floating numbers alpha and size

--- app/src/main/java/com/uravgcode/chooser/utils/Number.kt
+++ app/src/main/java/com/uravgcode/chooser/utils/Number.kt
@@ -10,24 +10,23 @@ class Number(private var x: Float, private var y: Float, color: Int, private val
 
     private val xOrigin = x
 
-    var alpha = 125f
+    var alpha = 255f
     private var alphaSpeed = 0f
 
     private var time = 0
 
     init {
-        size = 140f
         textPaint.color = color
         textPaint.textAlign = Paint.Align.CENTER
         textPaint.textSize = size
     }
 
     fun update(deltaTime: Int) {
-        alphaSpeed += deltaTime * 0.00003f
+        alphaSpeed += deltaTime * 0.00004f
         alpha -= alphaSpeed * deltaTime
-        alphaSpeed += deltaTime * 0.00003f
+        alphaSpeed += deltaTime * 0.00004f
 
-        textPaint.alpha = alpha.coerceIn(0f, 100f).toInt()
+        textPaint.alpha = alpha.coerceIn(0f, 255f).toInt()
 
         x = xOrigin + sin(time * 0.003).toFloat() * size * 0.25f
         y -= size * deltaTime * 0.0005f
