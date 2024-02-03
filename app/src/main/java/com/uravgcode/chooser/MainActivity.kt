ede8d3874f0f51a449294ab32686e88030a74188
Your Average Code <138674451+UrAvgCode@users.noreply.github.com>
2024-02-03 14:31:17 +0100

    persistent settings

--- app/src/main/java/com/uravgcode/chooser/MainActivity.kt
+++ app/src/main/java/com/uravgcode/chooser/MainActivity.kt
@@ -1,6 +1,8 @@
 package com.uravgcode.chooser
 
 import android.app.Activity
+import android.content.Context
+import android.content.SharedPreferences
 import android.os.Bundle
 import com.uravgcode.chooser.Chooser.Mode.GROUP
 import com.uravgcode.chooser.Chooser.Mode.ORDER
@@ -9,6 +11,7 @@ import com.uravgcode.chooser.databinding.ActivityMainBinding
 
 class MainActivity : Activity() {
     private lateinit var binding: ActivityMainBinding
+    private lateinit var preferences: SharedPreferences
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -16,37 +19,33 @@ class MainActivity : Activity() {
         setContentView(binding.root)
         binding.chooser.motionLayout = binding.motionLayout
 
+        preferences = getSharedPreferences("Settings", Context.MODE_PRIVATE)
+        loadPreferences()
+
         binding.btnCount.setOnClickListener {
             binding.chooser.apply {
-                count = when(mode) {
+                count = when (mode) {
                     SINGLE -> count % 5 + 1
                     GROUP -> (count - 1) % 4 + 2
                     ORDER -> 1
                 }
-                binding.btnCount.text = count.toString()
+
+                updateCount()
             }
         }
 
         binding.btnMode.setOnClickListener {
             if (binding.motionLayout.currentState != -1) {
                 binding.chooser.apply {
-
                     mode = when (mode) {
                         SINGLE -> GROUP
                         GROUP -> ORDER
                         ORDER -> SINGLE
                     }
 
-                    val drawable = when (mode) {
-                        SINGLE -> R.drawable.single_icon
-                        GROUP -> R.drawable.group_icon
-                        ORDER -> R.drawable.order_icon
-                    }
-
                     count = if (mode == GROUP) 2 else 1
-                    binding.motionLayout.transitionToState(if (mode == ORDER) R.id.hideCounter else R.id.start)
-                    binding.btnMode.foreground = context.getDrawable(drawable)
-                    binding.btnCount.text = count.toString()
+                    updateMode()
+                    updateCount()
                 }
             }
         }
@@ -56,5 +55,33 @@ class MainActivity : Activity() {
             true
         }
 
+    }
+
+    private fun loadPreferences() {
+        val preferences = getSharedPreferences("Settings", Context.MODE_PRIVATE)
+        binding.chooser.apply {
+            count = preferences.getInt("count", 1)
+            mode = Chooser.Mode.valueOf(preferences.getString("mode", "SINGLE")!!)
+            updateMode()
+            updateCount()
+        }
+    }
+
+    private fun updateMode() {
+        val mode = binding.chooser.mode
+        val drawable = when (mode) {
+            SINGLE -> R.drawable.single_icon
+            GROUP -> R.drawable.group_icon
+            ORDER -> R.drawable.order_icon
+        }
+        binding.btnMode.foreground = getDrawable(drawable)
+        preferences.edit().putString("mode", mode.toString()).apply()
+        binding.motionLayout.transitionToState(if (mode == ORDER) R.id.hideCounter else R.id.start)
+    }
+
+    private fun updateCount() {
+        val count = binding.chooser.count
+        binding.btnCount.text = count.toString()
+        preferences.edit().putInt("count", count).apply()
     }
 }
