4a65d05f7a4e270a90b29c80986d33d31788308c
Your Average Code <138674451+UrAvgCode@users.noreply.github.com>
2024-11-19 22:57:52 +0100

    update dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,11 +1,12 @@
 plugins {
     alias(libs.plugins.android.application)
-    alias(libs.plugins.jetbrains.kotlin.android)
+    alias(libs.plugins.kotlin.android)
+    alias(libs.plugins.kotlin.compose)
 }
 
 android {
     namespace = "com.uravgcode.chooser"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "com.uravgcode.chooser"
