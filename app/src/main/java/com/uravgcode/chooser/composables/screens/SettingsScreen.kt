695a0fb03b57e44243e5816baae309d9fdd4e3b0
Your Average Code <138674451+UrAvgCode@users.noreply.github.com>
2025-02-03 23:33:07 +0100

    add copyright and licence notice to SettingsRowSwitch

--- app/src/main/java/com/uravgcode/chooser/composables/screens/SettingsScreen.kt
+++ app/src/main/java/com/uravgcode/chooser/composables/screens/SettingsScreen.kt
@@ -37,7 +37,6 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.uravgcode.chooser.composables.settings.SettingsRowSwitch
