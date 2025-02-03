7807c69ce9634f898b4f20183ae49055fec5703f
Your Average Code <138674451+UrAvgCode@users.noreply.github.com>
2025-02-03 20:52:21 +0100

    remove import context

--- app/src/main/java/com/uravgcode/chooser/MainActivity.kt
+++ app/src/main/java/com/uravgcode/chooser/MainActivity.kt
@@ -15,7 +15,6 @@
 
 package com.uravgcode.chooser
 
-import android.content.Context
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
