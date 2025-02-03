fe27e6580ae73ab8b16648f620ed7633ff82327a
Your Average Code <138674451+UrAvgCode@users.noreply.github.com>
2025-02-03 20:58:06 +0100

    remove test dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -63,11 +63,6 @@ dependencies {
     implementation(libs.androidx.ui.graphics)
     implementation(libs.androidx.ui.tooling.preview)
     implementation(libs.androidx.material3)
-    testImplementation(libs.junit)
-    androidTestImplementation(libs.androidx.junit)
-    androidTestImplementation(libs.androidx.espresso.core)
-    androidTestImplementation(platform(libs.androidx.compose.bom))
-    androidTestImplementation(libs.androidx.ui.test.junit4)
     debugImplementation(libs.androidx.ui.tooling)
     debugImplementation(libs.androidx.ui.test.manifest)
 }
