a4f21620fb3bb0f441d050323fda11343c3da407
UrAvgCode <138674451+UrAvgCode@users.noreply.github.com>
2025-02-19 21:45:52 +0100

    version bump for pre release and added changelogs

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "com.uravgcode.chooser"
         minSdk = 26
         targetSdk = 35
-        versionCode = 10
-        versionName = "1.3.2"
+        versionCode = 11
+        versionName = "1.4-pre-1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
