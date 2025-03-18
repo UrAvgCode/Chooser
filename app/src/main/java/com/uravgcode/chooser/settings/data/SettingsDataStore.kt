ac847d59c87d0c4e6246e2ec9c17c697ecaa2aa6
UrAvgCode <138674451+UrAvgCode@users.noreply.github.com>
2025-03-18 21:27:36 +0100

    add comments to settings data files

--- app/src/main/java/com/uravgcode/chooser/settings/data/SettingsDataStore.kt
+++ app/src/main/java/com/uravgcode/chooser/settings/data/SettingsDataStore.kt
@@ -1,3 +1,18 @@
+/*
+ * Copyright (C) 2025 UrAvgCode
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <https://www.gnu.org/licenses/>.
+ *
+ * @author UrAvgCode
+ * @description DataStore extension for persisting application settings
+ */
+
 package com.uravgcode.chooser.settings.data
 
 import android.content.Context
