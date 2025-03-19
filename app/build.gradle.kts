502763c9def8ec7d3f0a94d5671d6b494c035533
UrAvgCode <138674451+UrAvgCode@users.noreply.github.com>
2025-03-19 16:49:26 +0100

    update and organize dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -63,12 +63,14 @@ dependencies {
     implementation(libs.androidx.activity.compose)
     implementation(platform(libs.androidx.compose.bom))
 
-    implementation(libs.androidx.navigation.compose)
+    implementation(libs.androidx.material3)
+    implementation(libs.androidx.material.icons.extended)
 
     implementation(libs.androidx.ui)
     implementation(libs.androidx.ui.graphics)
     implementation(libs.androidx.ui.tooling.preview)
-    implementation(libs.androidx.material3)
+
+    implementation(libs.androidx.navigation.compose)
 
     debugImplementation(libs.androidx.ui.tooling)
     debugImplementation(libs.androidx.ui.test.manifest)
