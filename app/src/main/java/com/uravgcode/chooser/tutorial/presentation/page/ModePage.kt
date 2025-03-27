d7bd8ce794ed82e747f56cb25165326c90744fad
UrAvgCode <138674451+UrAvgCode@users.noreply.github.com>
2025-03-27 15:14:06 +0100

    move animated preview to mode page

--- app/src/main/java/com/uravgcode/chooser/tutorial/presentation/page/ModePage.kt
+++ app/src/main/java/com/uravgcode/chooser/tutorial/presentation/page/ModePage.kt
@@ -1,9 +1,15 @@
 package com.uravgcode.chooser.tutorial.presentation.page
 
 import androidx.annotation.DrawableRes
+import androidx.compose.animation.graphics.ExperimentalAnimationGraphicsApi
+import androidx.compose.animation.graphics.res.animatedVectorResource
+import androidx.compose.animation.graphics.res.rememberAnimatedVectorPainter
+import androidx.compose.animation.graphics.vector.AnimatedImageVector
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
@@ -11,22 +17,37 @@ import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
-import com.uravgcode.chooser.tutorial.presentation.animation.AnimatedPreview
+import kotlinx.coroutines.delay
 
 @Composable
+@OptIn(ExperimentalAnimationGraphicsApi::class)
 fun ModePage(
     @DrawableRes iconId: Int,
     @DrawableRes previewId: Int,
     title: String,
     description: String,
-    previewDescription: String
+    previewDescription: String,
 ) {
+    val image = AnimatedImageVector.animatedVectorResource(previewId)
+    var atEnd by remember { mutableStateOf(false) }
+
+    LaunchedEffect(Unit) {
+        delay(300)
+        atEnd = true
+    }
+
     Column(
         modifier = Modifier
             .fillMaxWidth()
@@ -53,7 +74,14 @@ fun ModePage(
             textAlign = TextAlign.Center
         )
 
-        AnimatedPreview(previewId)
+        Image(
+            painter = rememberAnimatedVectorPainter(image, atEnd),
+            contentDescription = null,
+            modifier = Modifier
+                .fillMaxWidth(0.8f)
+                .aspectRatio(1f),
+            contentScale = ContentScale.Crop
+        )
 
         Text(
             text = previewDescription,
