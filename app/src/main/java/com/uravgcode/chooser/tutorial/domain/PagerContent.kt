80287044a871e5356cf3615d3ccaaa1dbb12b717
UrAvgCode <138674451+UrAvgCode@users.noreply.github.com>
2025-04-09 18:07:25 +0200

    add copyright notice to tutorial-related files

--- app/src/main/java/com/uravgcode/chooser/tutorial/domain/PagerContent.kt
+++ app/src/main/java/com/uravgcode/chooser/tutorial/domain/PagerContent.kt
@@ -1,3 +1,18 @@
+/*
+ * Copyright (C) 2025 UrAvgCode
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <https://www.gnu.org/licenses/>.
+ *
+ * @author UrAvgCode
+ * @description A list of page data for the tutorial pager.
+ */
+
 package com.uravgcode.chooser.tutorial.domain
 
 import com.uravgcode.chooser.R
--- app/src/main/java/com/uravgcode/chooser/tutorial/presentation/component/PageIndicator.kt
+++ app/src/main/java/com/uravgcode/chooser/tutorial/presentation/component/PageIndicator.kt
@@ -1,3 +1,18 @@
+/*
+ * Copyright (C) 2025 UrAvgCode
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <https://www.gnu.org/licenses/>.
+ *
+ * @author UrAvgCode
+ * @description PageIndicator shows the current page position with animated indicators.
+ */
+
 package com.uravgcode.chooser.tutorial.presentation.component
 
 import androidx.compose.animation.core.animateDpAsState
