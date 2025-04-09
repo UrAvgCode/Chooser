c449f3d44a15834743fad46d85077c54a58bb589
UrAvgCode <138674451+UrAvgCode@users.noreply.github.com>
2025-04-09 17:24:39 +0200

    version bump for release and added changelogs

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "com.uravgcode.chooser"
         minSdk = 26
         targetSdk = 35
-        versionCode = 14
-        versionName = "1.4.1"
+        versionCode = 15
+        versionName = "1.4.2"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
