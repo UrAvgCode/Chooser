aecf60cd2b95079bf6a0d63120def387e019a2c7
UrAvgCode <138674451+UrAvgCode@users.noreply.github.com>
2025-04-10 19:25:28 +0200

    add copyright notices to settings button files

--- app/src/main/java/com/uravgcode/chooser/settings/presentation/button/SettingsButton.kt
+++ app/src/main/java/com/uravgcode/chooser/settings/presentation/button/SettingsButton.kt
@@ -1,3 +1,18 @@
+/*
+ * Copyright (C) 2025 UrAvgCode
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <https://www.gnu.org/licenses/>.
+ *
+ * @author UrAvgCode
+ * @description SettingsButton is a reusable button component for the settings screen.
+ */
+
 package com.uravgcode.chooser.settings.presentation.button
 
 import androidx.compose.foundation.layout.fillMaxWidth
--- app/src/main/java/com/uravgcode/chooser/settings/presentation/button/SettingsButtonImport.kt
+++ app/src/main/java/com/uravgcode/chooser/settings/presentation/button/SettingsButtonImport.kt
@@ -1,3 +1,18 @@
+/*
+ * Copyright (C) 2025 UrAvgCode
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <https://www.gnu.org/licenses/>.
+ *
+ * @author UrAvgCode
+ * @description SettingsButtonImport provides functionality to import settings from a previously exported file.
+ */
+
 package com.uravgcode.chooser.settings.presentation.button
 
 import android.widget.Toast
