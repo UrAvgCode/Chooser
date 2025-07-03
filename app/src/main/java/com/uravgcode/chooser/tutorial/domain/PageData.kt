e09352bf9d2688f6aed34999a776a62105c2c4c9
UrAvgCode <138674451+UrAvgCode@users.noreply.github.com>
2025-07-03 13:53:55 +0200

    use param annotation for icon and preview id

--- app/src/main/java/com/uravgcode/chooser/tutorial/domain/PageData.kt
+++ app/src/main/java/com/uravgcode/chooser/tutorial/domain/PageData.kt
@@ -18,8 +18,8 @@ package com.uravgcode.chooser.tutorial.domain
 import androidx.annotation.DrawableRes
 
 data class PageData(
-    @DrawableRes val iconId: Int? = null,
-    @DrawableRes val previewId: Int,
+    @param:DrawableRes val iconId: Int? = null,
+    @param:DrawableRes val previewId: Int,
     val title: String,
     val description: String,
 )
