cbd27e9d1d47c4d64cda69157568297ca5f8c544
UrAvgCode <dev@uravgcode.com>
2025-08-11 23:44:00 +0200

    version bump for release and added changelogs

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
     defaultConfig {
         minSdk = 26
         targetSdk = 36
-        versionCode = 20
-        versionName = "1.4.6"
+        versionCode = 21
+        versionName = "1.4.7"
     }
 
     flavorDimensions.add("distribution")
