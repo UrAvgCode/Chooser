c5d65b6d18f67b5538138f933c411fb65b78b731
UrAvgCode <dev@uravgcode.com>
2026-06-09 11:14:48 +0200

    update sdk, gradle wrapper and other dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -8,14 +8,14 @@ android {
     namespace = "com.uravgcode.chooser"
 
     compileSdk {
-        version = release(36) {
-            minorApiLevel = 1
+        version = release(37) {
+            minorApiLevel = 0
         }
     }
 
     defaultConfig {
         minSdk = 26
-        targetSdk = 36
+        targetSdk = 37
         versionCode = 25
         versionName = "1.4.11"
     }
